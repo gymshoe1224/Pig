8ac521907b962e28d84fcc379a255eefa7c45846
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2022-11-11 09:09:53 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Chris Markiewicz on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
