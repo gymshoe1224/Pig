66aef01b815df590e5ec7549e09c8a04bfd9b9a4
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2022-11-11 10:19:03 -0600

    finished game

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var gameScore = 0
     @State private var randomValue = 0
     @State private var rotation = 0.0
+    @State private var gameOver = false
     var body: some View {
         NavigationView {
             ZStack {
@@ -40,6 +41,10 @@ struct ContentView: View {
                             withAnimation(.easeInOut(duration: 1)) {
                                 rotation += 360
                             }
+                            if gameScore >= 100
+                            {
+                                gameOver = true
+                            }
                         }
                         .buttonStyle(CustomButtonStyle())
                     }
@@ -47,9 +52,22 @@ struct ContentView: View {
                     NavigationLink("How To Play", destination: InstructionsView())
                         .font(Font.custom("Marker Felt", size: 24))
                         .padding()
+                    Button("Reset") {
+                        gameScore = 0
+                        endTurn()
+                    }
+                    .font(Font.custom("Marker Felt", fixedSize: 24))
                     Spacer()
                 }
             }
+            alert(isPresented: $gameOver, content: {
+                Alert(title: Text("You won the game!"), dismissButton: .destructive(Text("Play again"), action: {
+                    withAnimation(Animation.default) {
+                        gameScore = 0
+                        gameOver = false
+                    }
+                }))
+            })
         }
     }
     func endTurn() {
